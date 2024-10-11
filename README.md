805ea80caa2ba681cb4446a20d3712ef63df82ed
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-11 10:30:53 +0000

    initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# hj
+# Android device tree for samsung SM-A055F (a05m)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_a05m.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from a05m device
+$(call inherit-product, device/samsung/a05m/device.mk)
+
+PRODUCT_DEVICE := a05m
+PRODUCT_NAME := lineage_a05m
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A055F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a05mxx-user 14 UP1A.231005.007 A055FXXU5CXH2 release-keys"
+
+BUILD_FINGERPRINT := samsung/a05mxx/a05m:14/UP1A.231005.007/A055FXXU5CXH2:user/release-keys
