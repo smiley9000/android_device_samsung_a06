e14e7d175debd78adff253b7b98b47fb931b6359
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-17 20:44:15 +0530

    a05m: typo

--- releasetools.py
+++ releasetools.py
@@ -36,4 +36,3 @@ def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
-  returnreleasetools.py
