db41a5e62b9fbbb446ee99fbe5887ff2c1434afe
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-28 13:32:09 +0530

    Update lineage_a05m.mk

--- lineage_a05m.mk
+++ lineage_a05m.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a05m/device.mk)
 
+# Inherit from wingcamera-samsung
+$(call inherit-product, vendor/samsung/wing-camera/wingcamera-samsung.mk)
+
 PRODUCT_DEVICE := a05m
 PRODUCT_NAME := lineage_a05m
 PRODUCT_BRAND := Samsung
@@ -21,11 +24,12 @@ PRODUCT_MODEL := Galaxy A05
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+PRODUCT_NO_CAMERA := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a05mxx-user 14 UP1A.231005.007 A055FXXS6CXI2 release-keys"
 
 BUILD_FINGERPRINT := samsung/a05mxx/a05m:14/UP1A.231005.007/A055FXXS6CXI2:user/release-keys
 
-#PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
-#     ro.debuggable=0
+PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
+     ro.debuggable=0
