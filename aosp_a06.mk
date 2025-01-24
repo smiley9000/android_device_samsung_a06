3c37fdab19a1d4118c8f39ae85ffc88f98046478
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-01-24 22:23:06 +0530

    Update and rename lineage_a06.mk to aosp_a06.mk

--- aosp_a06.mk
+++ aosp_a06.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from a06 device
 $(call inherit-product, device/samsung/a06/device.mk)
@@ -17,10 +17,10 @@ $(call inherit-product, device/samsung/a06/device.mk)
 # Inherit from wingcamera-samsung
 
 PRODUCT_DEVICE := a06
-PRODUCT_NAME := lineage_a06
+PRODUCT_NAME := aosp_a06
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A06
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
